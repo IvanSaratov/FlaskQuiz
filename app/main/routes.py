9e89071ce20ead9b15e5e8c8d229dbc176ada64f
ivan <dfeqcn11@gmail.com>
2021-03-23 00:40:27 +0400

    ..

--- app/main/routes.py
+++ app/main/routes.py
@@ -2,17 +2,25 @@ from flask import render_template
 from flask_login import login_required
 
 from app.main import bp
-from app.models import User
+from app.models import User, Quiz
 
 
 @bp.route('/')
 @bp.route('/index')
 @login_required
 def index():
-    return render_template('index.html', title='Домашняя страница')
+    return render_template('index.html', title='Главная страница')
 
-# @bp.route('/rating')
-# @login_required
-# def rating():
-#     users = User.query.order_by(User.score).all()
-#     return render_template('rating.html', title='Рейтинг', users=users)
+
+@bp.route('/rating')
+@login_required
+def rating():
+    users = User.query.order_by(User.rating).all()
+    return render_template('rating.html', title='Счет', users=users)
+
+
+@bp.route('/quizs')
+@login_required
+def list():
+    quizs = Quiz.query.order_by(Quiz.name).all()
+    return render_template('list.html', title='Список игр', quizs=quizs)
